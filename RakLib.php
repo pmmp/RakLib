1fee79e4aac0f958ab720606e223b0aa690f5c47
Dylan K. Taylor <odigiman@gmail.com>
2017-08-24 19:25:28 +0100

    Bump version to 0.8.1

--- RakLib.php
+++ RakLib.php
@@ -49,7 +49,7 @@ if($errors > 0){
 unset($errors);
 
 abstract class RakLib{
-	const VERSION = "0.8.0";
+	const VERSION = "0.8.1";
 	const PROTOCOL = 6;
 	const MAGIC = "\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78";
 
