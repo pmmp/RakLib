a832f3fa50ba48d8cfc01f725b8a2ff99718450e
Dylan K. Taylor <odigiman@gmail.com>
2017-10-11 12:31:54 +0100

    Use empty braces on these loops so nobody makes the horrible mistake I almost just made because of fucking PhpStorm

--- server/SessionManager.php
+++ server/SessionManager.php
@@ -116,8 +116,8 @@ class SessionManager{
 		while(!$this->shutdown){
 			$start = microtime(true);
 			$max = 5000;
-			while(--$max and $this->receivePacket());
-			while($this->receiveStream());
+			while(--$max and $this->receivePacket()){}
+			while($this->receiveStream()){}
 			$time = microtime(true) - $start;
 			if($time < 0.05){
 				time_sleep_until(microtime(true) + 0.05 - $time);
