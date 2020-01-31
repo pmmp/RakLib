079d0d5d9c7eea089f078ee8c8681ae4ca2c49c1
Dylan K. Taylor <odigiman@gmail.com>
2020-01-31 14:57:10 +0000

    Packet: fix type violation in clean()

--- src/protocol/Packet.php
+++ src/protocol/Packet.php
@@ -111,7 +111,7 @@ abstract class Packet extends BinaryStream{
 	abstract protected function decodePayload() : void;
 
 	public function clean(){
-		$this->buffer = null;
+		$this->buffer = "";
 		$this->offset = 0;
 		$this->sendTime = null;
 
