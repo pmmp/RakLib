e64eb30ce5c3c1fe9ac29353823f68b26c408151
Dylan K. Taylor <odigiman@gmail.com>
2020-03-29 09:48:14 +0100

    Get rid of the hand-written dependency checking this should be done by composer now.

--- src/RakLib.php
+++ src/RakLib.php
@@ -17,56 +17,6 @@ declare(strict_types=1);
 
 namespace raklib;
 
-use function defined;
-use function extension_loaded;
-use function phpversion;
-use function substr_count;
-use function version_compare;
-use const PHP_EOL;
-use const PHP_VERSION;
-
-//Dependencies check
-$errors = 0;
-if(version_compare(RakLib::MIN_PHP_VERSION, PHP_VERSION) > 0){
-	echo "[CRITICAL] Use PHP >= " . RakLib::MIN_PHP_VERSION . PHP_EOL;
-	++$errors;
-}
-
-$exts = [
-	"bcmath" => "BC Math",
-	"pthreads" => "pthreads",
-	"sockets" => "Sockets"
-];
-
-foreach($exts as $ext => $name){
-	if(!extension_loaded($ext)){
-		echo "[CRITICAL] Unable to find the $name ($ext) extension." . PHP_EOL;
-		++$errors;
-	}
-}
-
-$pthreads_version = phpversion("pthreads");
-if($pthreads_version !== false){
-	if(substr_count($pthreads_version, ".") < 2){
-		$pthreads_version = "0.$pthreads_version";
-	}
-
-	if(version_compare($pthreads_version, "3.1.7dev") < 0){
-		echo "[CRITICAL] pthreads >= 3.1.7dev is required, while you have $pthreads_version.";
-		++$errors;
-	}
-}
-
-if(!defined('AF_INET6')){
-	echo "[CRITICAL] This build of PHP does not support IPv6. IPv6 support is required.";
-	++$errors;
-}
-
-if($errors > 0){
-	exit(1); //Exit with error
-}
-unset($errors, $exts);
-
 abstract class RakLib{
 	public const VERSION = "0.12.0";
 
