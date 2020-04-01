479f452fdd2bd11444b965c553c88b71e344c818
Frago9876543210 <al.volf.av@gmail.com>
2020-04-01 20:09:33 +0300

    Fixed codestyle of phpdoc

--- src/utils/InternetAddress.php
+++ src/utils/InternetAddress.php
@@ -18,18 +18,11 @@ declare(strict_types=1);
 namespace raklib\utils;
 
 class InternetAddress{
-
-	/**
-	 * @var string
-	 */
+	/** @var string */
 	public $ip;
-	/**
-	 * @var int
-	 */
+	/** @var int */
 	public $port;
-	/**
-	 * @var int
-	 */
+	/** @var int */
 	public $version;
 
 	public function __construct(string $address, int $port, int $version){
