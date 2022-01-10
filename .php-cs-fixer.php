61a7cc25d22601fd56b3c182e95b31c4c2873ebf
Dylan K. Taylor <dktapps@pmmp.io>
2022-01-10 21:28:33 +0000

    Update license headers

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -37,6 +37,20 @@ return (new PhpCsFixer\Config)
 			'import_functions' => true,
 			'import_classes' => null,
 		],
+		'header_comment' => [
+			'header' => <<<COMMENT
+This file is part of RakLib.
+Copyright (C) 2014-2022 PocketMine Team <https://github.com/pmmp/RakLib>
+
+RakLib is not affiliated with Jenkins Software LLC nor RakNet.
+
+RakLib is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+COMMENT,
+			'location' => 'after_open',
+		],
 		'indentation_type' => true,
 		'native_function_invocation' => [
 			'scope' => 'namespaced',
--- src/protocol/EncapsulatedPacket.php
+++ src/protocol/EncapsulatedPacket.php
@@ -1,16 +1,15 @@
 <?php
 
 /*
- * RakLib network library
+ * This file is part of RakLib.
+ * Copyright (C) 2014-2022 PocketMine Team <https://github.com/pmmp/RakLib>
  *
+ * RakLib is not affiliated with Jenkins Software LLC nor RakNet.
  *
- * This project is not affiliated with Jenkins Software LLC nor RakNet.
- *
- * This program is free software: you can redistribute it and/or modify
+ * RakLib is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
  */
 
 declare(strict_types=1);
